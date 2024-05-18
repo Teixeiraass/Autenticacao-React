221b97df8af75e9ca90afd0cdc18c2c3008b74a6
Teixeiraass <guist2017@hotmail.com>
2024-05-18 10:36:16 -0300

    feat: add tests for Login Page
    this commit create new tests for the login page, exemple, login button
    and redirection

---
+++ src/test/Home.spec.tsx
@@ -0,0 +1,5 @@
+describe('HomePage testes', () => {
+    it("", () => {
+        expect(1 + 1).toBe(2)
+    })
+})
--- src/test/Login.spec.tsx
+++ src/test/Login.spec.tsx
@@ -1,9 +1,9 @@
-import { screen, render, fireEvent } from '@testing-library/react'
+import { screen, render, fireEvent, waitFor } from '@testing-library/react'
 import { AuthProvider } from '../context/AuthProvider';
 import LoginPage from '../page/Login';
 import { MemoryRouter } from 'react-router-dom';
 
-describe('LoginPage', () => {
+describe('LoginPage testes', () => {
     it('renders email and password fields', () => {
         render(
             <MemoryRouter>
@@ -43,5 +43,70 @@ describe('LoginPage', () => {
         const passwordInput = screen.getByPlaceholderText('**********') as HTMLInputElement;
         fireEvent.change(passwordInput, { target: { value: 'password' } });
         expect(passwordInput.value).toBe("password")
+    });
+
+    it('verify if error message is rendering', () => {
+        render(
+            <MemoryRouter>
+                <AuthProvider>
+                    <LoginPage />
+                </AuthProvider>
+            </MemoryRouter>
+        );
+
+        const emailInput = screen.getByPlaceholderText('**********') as HTMLInputElement;
+        const passwordInput = screen.getByPlaceholderText('**********') as HTMLInputElement;
+        fireEvent.change(passwordInput, { target: { value: 'password' } });
+        fireEvent.change(emailInput, { target: { value: '1234@email.com' } });
+        waitFor(() => {
+            expect(screen.getByText("Usuário e/ou senha incorreto(s)")).toBeInTheDocument()
+        })
+    });
+
+    it('submits login on button click', () => {
+        const handleLogin = jest.fn();
+        render(
+            <MemoryRouter>
+                <AuthProvider>
+                    <LoginPage />
+                </AuthProvider>
+            </MemoryRouter>
+        );
+
+        const LoginButton = screen.getByRole('button', { name: 'Sing in' })
+        fireEvent.click(LoginButton);
+        waitFor(() => {
+            expect(handleLogin).toHaveBeenCalledTimes(1)
+        })
+    });
+
+    it('redirects to home page after successful login', async () => {
+        render(
+            <MemoryRouter>
+                <AuthProvider>
+                    <LoginPage />
+                </AuthProvider>
+            </MemoryRouter>
+        );
+
+        const navigateMock = jest.fn();
+        jest.mock('react-router-dom', () => ({
+            useNavigate: () => navigateMock,
+        }));
+
+
+
+        const emailInput = screen.getByPlaceholderText('@gmail.com');
+        const passwordInput = screen.getByPlaceholderText('**********');
+        const LoginButton = screen.getByRole('button', { name: 'Sing in' })
+
+        fireEvent.change(emailInput, { target: { value: 'cliente@youdrive.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'password' } });
+        fireEvent.click(LoginButton);
+
+        waitFor(() => {
+            expect(navigateMock).toHaveBeenCalledWith('/');
+        })
+
     });
 })
