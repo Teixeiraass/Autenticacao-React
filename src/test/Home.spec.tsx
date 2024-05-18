c9108574ebd53dc104a5d82380dad1be750d7b24
Teixeiraass <guist2017@hotmail.com>
2024-05-18 11:24:02 -0300

    feat: add test Home page user information

--- src/test/Home.spec.tsx
+++ src/test/Home.spec.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react"
+import {render, screen, waitFor } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 import { AuthProvider } from "../context/AuthProvider"
 import HomePage from "../page/Home"
@@ -16,44 +16,6 @@ describe('HomePage testes', () => {
         waitFor(() => {
             expect(screen.getByDisplayValue("Cliente")).toBeInTheDocument()
             expect(screen.getByDisplayValue("cliente@youdrive.com")).toBeInTheDocument()
-        })
-    })
-
-    it('submits login on button click', () => {
-        const handleLogout = jest.fn();
-        render(
-            <MemoryRouter>
-                <AuthProvider>
-                    <HomePage />
-                </AuthProvider>
-            </MemoryRouter>
-        );
-
-        waitFor(() => {
-            const Logout = screen.getByRole('button', { name: 'Logout' })
-            fireEvent.click(Logout);
-            expect(handleLogout).toHaveBeenCalledTimes(1)
-        })
-    });
-
-    it('redirects to home page after successful login', async () => {
-        render(
-            <MemoryRouter>
-                <AuthProvider>
-                    <HomePage />
-                </AuthProvider>
-            </MemoryRouter>
-        );
-
-        const navigateMock = jest.fn();
-        jest.mock('react-router-dom', () => ({
-            useNavigate: () => navigateMock,
-        }));
-        
-        waitFor(() => {
-            const Logout = screen.getByRole('button', { name: 'Logout' })
-            fireEvent.click(Logout);
-            expect(navigateMock).toHaveBeenCalledWith('/login');
         })
     });
 })
